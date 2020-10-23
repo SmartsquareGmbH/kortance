cf136fd5a2a69ab20fb1ba90579d4ea0f6f9fb2a
deen13 <dierkes@smartsquare.de>
2020-10-23 16:40:36 +0200

    Organize imports

--- src/main/kotlin/de/smartsquare/kortance/MqttCommand.kt
+++ src/main/kotlin/de/smartsquare/kortance/MqttCommand.kt
@@ -2,7 +2,6 @@ package de.smartsquare.kortance
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
-import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.int

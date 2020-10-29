c7ee21e3009db575180d3a212ccc34b410bb7d70
deen13 <dierkes@smartsquare.de>
2020-10-29 14:58:49 +0100

    Bump version

--- src/main/kotlin/de/smartsquare/kortance/Kortance.kt
+++ src/main/kotlin/de/smartsquare/kortance/Kortance.kt
@@ -8,7 +8,7 @@ import java.util.concurrent.Callable
 
 @CommandLine.Command(
     name = "kortance",
-    version = ["0.4.0"],
+    version = ["0.7.0"],
     subcommands = [StressCommand::class, SpikeCommand::class, SoakCommand::class],
     mixinStandardHelpOptions = true
 )
